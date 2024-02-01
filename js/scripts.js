239aa9633a0784ddbcc44f7cd8c8ab059b951dfd
Wanderson Rodrigues <wand_henrique@hotmail.com>
2024-02-01 19:28:00 -0300

    create show password

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
-const navRight = document.getElementById('nav-right');
-const navLeft = document.getElementById('nav-left');
+const navRight = document.getElementById("nav-right");
+const navLeft = document.getElementById("nav-left");
 
 function toggleMenuRight(active) {
-    if (active) {
-        navRight.classList.add('active-right');
-    } else {
-        navRight.classList.remove('active-right');
-    }
+  if (active) {
+    navRight.classList.add("active-right");
+  } else {
+    navRight.classList.remove("active-right");
+  }
 }
-
 
 function toggleMenuLeft(active) {
-    if (active) {
-        navLeft.classList.add('active-left');
-    } else {
-        navLeft.classList.remove('active-left');
-    }
+  if (active) {
+    navLeft.classList.add("active-left");
+  } else {
+    navLeft.classList.remove("active-left");
+  }
 }
+
+// Mostrar Senha
+
+const campoSenha = document.querySelector(".campo-senha");
+const btnMostarSenha = document.querySelector(".btn-mostrar-senha");
+const campoRepetirSenha = document.querySelector(".campo-repetir-senha");
+const btnMostarRepetirSenha = document.querySelector(".btn-mostrar-repetir-senha");
+
+btnMostarSenha.addEventListener("change", () => {
+  const estadoAtualDoCampoDESenha =
+    campoSenha.getAttribute("type") === "password" ? "text" : "password";
+
+  campoSenha.setAttribute("type", estadoAtualDoCampoDESenha);
+});
+
+btnMostarRepetirSenha.addEventListener("change", () => {
+  const estadoAtualDoCampoRepetirSenha =
+    campoRepetirSenha.getAttribute("type") === "password" ? "text" : "password";
+
+  campoRepetirSenha.setAttribute("type", estadoAtualDoCampoRepetirSenha);
+});
