bc86b6cd36b8f7239eeb66f6a04b250b96abbf9e
Wanderson Rodrigues <wand_henrique@hotmail.com>
2024-02-05 18:29:54 -0300

    Att. responsividade

---
+++ js/notification.js
@@ -0,0 +1,9 @@
+const  notificationLeft = document.querySelector('.notificacao-left')
+
+
+const name = ["lucas459", "ana871", "leo2346", "tina789", "samuel456", "clara987", "henry345", "bela234", "felix567", "mila789", "david123", "lara890", "bruno567", "zara234", "carol789", "luan456", "maria890", "pedro567", "lola234", "ricco789", "aline456", "guto890", "nina567", "vini234", "luma789", "rico456", "sara890", "pablo567", "mia234", "felipe789"]
+
+const value = ["Ganhou R$ 15.25", "Ganhou R$ 38.50", "Ganhou R$ 64.75", "Ganhou R$ 22.10", "Ganhou R$ 57.89",  "Ganhou R$ 49.45", "Ganhou R$ 14.78", "Ganhou R$ 83.20", "Ganhou R$ 36.45", "Ganhou R$ 91.60",  "Ganhou R$ 18.30", "Ganhou R$ 75.40", "Ganhou R$ 42.65", "Ganhou R$ 29.80", "Ganhou R$ 68.90",  "Ganhou R$ 53.15", "Ganhou R$ 26.70", "Ganhou R$ 81.95", "Ganhou R$ 44.50", "Ganhou R$ 19.85",  "Ganhou R$ 66.25", "Ganhou R$ 31.40", "Ganhou R$ 58.55", "Ganhou R$ 47.10", "Ganhou R$ 12.35",  "Ganhou R$ 93.70", "Ganhou R$ 35.25", "Ganhou R$ 72.80", "Ganhou R$ 24.95", "Ganhou R$ 87.30"]
+
+
+
